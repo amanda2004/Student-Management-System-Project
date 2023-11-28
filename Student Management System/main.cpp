2bc0b3f26acc993725166ce89d6417f11e8f4f60
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-11-28 18:41:00 +0700

    add search and display function

--- Student Management System/main.cpp
+++ Student Management System/main.cpp
@@ -3,8 +3,28 @@
 
 using namespace std;
 
+// Function to search for a student by ID and display the information
+void searchStudent(Student* node, int student_id) {
+    while (node != nullptr) {
+        if (student_id == node->student_id) {
+            cout << "Student found:" << endl;
+            cout << setw(15) << "Name" << setw(10) << "Student ID" << setw(15) << "Study Program" << setw(10) << "Cohort" << setw(10) << "GPA" << setw(15) << "Date of Birth" << setw(15) << "Phone Number" << endl;
+            cout << setfill('-') << setw(85) << "" << setfill(' ') << endl;
+            cout << setw(15) << node->name << setw(10) << node->student_id << setw(15) << node->study_program << setw(10) << node->cohort << setw(10) << fixed << setprecision(2) << node->gpa << setw(15) << node->date_of_birth << setw(15) << node->phone_number << endl;
+            return;
+        } else if (student_id < node->student_id) {
+            node = node->left;
+        } else {
+            node = node->right;
+        }
+    }
+    cout << "Student not found." << endl;
+}
+
 int main()
 {
+    StudentManagementSystem system;
+
     int choice;
     do{
         cout << "\n----------------------------------------------------------------------------------------------------------"
@@ -29,53 +49,68 @@ int main()
                     int cohort;
                     float gpa;
                     string date_of_birth;
-                    int phone_number
+                    string phone_number;
+
                     cout << "Enter Student Name: ";
                     cin.ignore(); // Clear the buffer
                     getline(cin, name);
+
                     cout << "Enter Student ID: ";
                     cin >> student_id;
+
                     cout << "Enter Study Program: ";
-                    cin >> study_program;
+                    cin.ignore(); //Clear the buffer
+                    getline(cin, study_program);
+
                     cout << "Enter Student Cohort: ";
                     cin >> cohort;
+
                     cout << "Enter Student GPA: ";
                     cin >> gpa;
+
                     cout << "Enter Student Date Of Birth: ";
-                    cin >> date_of_birth;
+                    cin.ignore(); //Clear the buffer
+                    getline(cin, date_of_birth);
+
                     cout << "Enter Student Phone Number: ";
-                    cin >> phone_number;
+                    getline(cin, phone_number);
+
                     system.addStudent(name, student_id, study_program, cohort, gpa, date_of_birth, phone_number);
                 }
                 break;
+
             case 2:
-                {
-                    system.editStudent();
-                }
+                system.editStudent();
                 break;
+
             case 3:
                 {
-
+                    int student_id;
+                    cout << "Enter the Student ID to search: ";
+                    cin >> student_id;
+                    searchStudent(system.getRoot(), student_id);
                 }
                 break;
+
             case 4:
-                {
-
-                }
+                system.displayStudents();
                 break;
+
             case 5:
                 {
-                    int id;
+                    int student_id;
                     cout << "Enter the Student ID of the student you want to remove: ";
-                    cin >> id;
-                    system.removeStudent(id);
+                    cin >> student_id;
+                    system.removeStudent(student_id);
                 }
                 break;
-            case 6:
-                {
 
-                }
+            case 6:
+                system.clearTree(system.getRoot());
+                system.clearList();
+                cout << "Exiting program. Goodbye!" << endl;
                 break;
+
             default:
                 cout << "Invalid choice. Please enter a valid option.\n";
         }
