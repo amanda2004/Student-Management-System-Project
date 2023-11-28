92e321f42bffd11df0625a4071c21a7486542c07
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-11-28 18:40:28 +0700

    add search and display function

--- Student Management System/include/StudentManagementSystem.h
+++ Student Management System/include/StudentManagementSystem.h
@@ -1,24 +1,8 @@
 #ifndef STUDENTMANAGEMENTSYSTEM_H
 #define STUDENTMANAGEMENTSYSTEM_H
-#include <iostream>
-#include <iomanip>
+
+#include "Student.h"
 #include <string>
-
-using namespace std;
-
-//Structure to represent a student
-struct Student {
-    string name;
-    int student_id;
-    string study_program;
-    int cohort;
-    float gpa;
-    string date_of_birth;
-    int phone_number;
-    Student* left;
-    Student* right;
-    Student* next; //Linked list pointer
-};
 
 //Class for managing students using binary search tree and linked list
 class StudentManagementSystem
@@ -30,37 +14,34 @@ class StudentManagementSystem
         //Destructor to free memory
         ~StudentManagementSystem();
 
-        //Function to remove a student with a specific Student ID from the binary search tree
-        Student* removeStudent(Student* node, int student_id);
-
-        //Function to remove a node from the binary search tree
-        Student* removeNode(Student* node);
-
-        //Function to clear all students from the binary search tree
-        void clearTree(Student* node);
-
-        //Function to clear all students from the linked list
-        void clearList();
-
         //Wrapper function to remove a student
         void removeStudent(int student_id);
 
         //Getter function to access the root of the binary search tree
         Student* getRoot();
-        
-        //Function to add a new student to the binary search tree
-        Student* addStudent(Student* node, string name, int student_id, string study_program, int cohort, float gpa, string date_of_birth, int phone_number);
-        
+
         //Wrapper function to add a student
-        void addStudent(string name, int student_id, string study_program, int cohort, float gpa, string date_of_birth, int phone_number);
-        
+        void addStudent(string name, int student_id, string study_program, int cohort, float gpa, string date_of_birth, string phone_number);
+
         //Wrapper function to edit a student's information
         void editStudent();
-        
+
+        void displayStudents();
+        void clearTree(Student* node); //Function to clear all students from the binary search tree
+        void clearList(); //Function to clear all students from the linked list
+
     private:
         Student* root; //Root of the binary search tree
         Student* head; //Head of the linked list
+
+        Student* addStudent(Student* node, string name, int student_id, string study_program, int cohort, float gpa, string date_of_birth, string phone_number); //Function to add a new student to the binary search tree
         Student* chooseStudentToEdit(); // Function to choose a student to edit
+        Student* removeStudent(Student* node, int student_id); //Function to remove a student with a specific Student ID from the binary search tree
+        Student* removeNode(Student* node); //Function to remove a node from the binary search tree
+        Student* findMin(Student* node);
+        Student* searchStudent(Student* node, int student_id);
+        void displayStudentsList();
+        void displayStudentsTree(Student* node);
         void editStudent(Student* node, int student_id);
 };
 
